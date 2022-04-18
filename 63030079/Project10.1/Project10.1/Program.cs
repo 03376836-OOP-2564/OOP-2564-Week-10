87fa6895ee282d59b2951634bbb18e9a8289e28d
WeerawitPM <63030079@kmitl.ac.th>
2022-04-18 23:00:39 +0700

    Edit Project10.1

--- 63030079/Project10.1/Project10.1/Program.cs
+++ 63030079/Project10.1/Project10.1/Program.cs
@@ -7,10 +7,10 @@
         id = value;
         ShowId();
     }
-    internal void Set(string value)
+    internal void SetId2(string value)
     {
         id2 = value;
-        Show();
+        ShowId2();
     }
 
     internal void ShowId()
@@ -18,7 +18,7 @@
         Console.WriteLine($"id : hashcode = [{this.id.GetHashCode():X}], value = {id}");
     }
 
-    internal unsafe void Show()
+    internal unsafe void ShowId2()
     {
         Console.WriteLine($"id2 : hashcode = [{Student.id2.GetHashCode():X}], value = {id2}");
     }
@@ -43,22 +43,22 @@ class Program
 
         //  กำหนดและแสดงค่าใน member ของ s1
         s1.SetId(1001);
-        s1.Set("Computer Engineering");
+        s1.SetId2("Computer Engineering");
         Console.WriteLine();
 
         //  กำหนดและแสดงค่าใน member ของ s2
         s2.SetId(1002);
-        s2.Set("Electrical Engineering");
+        s2.SetId2("Electrical Engineering");
         Console.WriteLine();
 
         //  กำหนดและแสดงค่าใน member ของ s3
         s3.SetId(1003);
-        s3.Set("Mechanical Engineering");
+        s3.SetId2("Mechanical Engineering");
         Console.WriteLine();
 
         //  แสดงค่าใน static member ของ s1-s3 อีกครั้ง
-        Console.Write("S1."); s1.Show();
-        Console.Write("S2."); s2.Show();
-        Console.Write("S3."); s3.Show();
+        Console.Write("S1."); s1.ShowId2();
+        Console.Write("S2."); s2.ShowId2();
+        Console.Write("S3."); s3.ShowId2();
     }
 }
