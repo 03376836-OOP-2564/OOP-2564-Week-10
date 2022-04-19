6eac35c58a4ec0183fff240b405a20231918ba1b
WeerawitPM <63030079@kmitl.ac.th>
2022-04-19 23:33:40 +0700

    Add Project

---
+++ 63030079/Project10.2/Project10.2/Program.cs
@@ -0,0 +1,42 @@
+﻿//class Student
+//{
+//	static public int id;
+//	static public void PrintID()
+//	{
+//		Console.WriteLine($"student ID = {id}");
+//	}
+
+//}
+//class Program
+//{
+//	static void Main()
+//	{
+//		// เรียกใช้ได้โดยไม่ต้องสร้าง instance ของ Student
+//		Student.id = 1001;
+//		Student.PrintID();
+//	}
+//}
+
+//////////////////////
+
+class Student
+{
+    public int id;
+    public void PrintID()
+    {
+        Console.WriteLine($"student ID = {id}");
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+        // เรียกใช้ได้โดยสร้าง instance ของ Student
+        Student stu = new Student();
+        stu.id = 1002;
+        stu.PrintID();
+    }
+}
+
+
