dc69ca434326cbd42e1fdbe1dd2b1b2c50b8009d
Piyachat <63030052@kmitl.ac.th>
2022-05-08 19:30:43 +0700

    add code

--- 63030052/Project 10.6/Project 10.6/Program.cs
+++ 63030052/Project 10.6/Project 10.6/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Project_10._6
+class Person
 {
-    internal class Program
+    private int age;       // years
+    const int MaxAge = 120;
+    const int MinAge = 0;
+    public int Age
     {
-        static void Main(string[] args)
+        get { return age; }
+        set
         {
+            if (value > MaxAge)
+            {
+                Console.WriteLine($"Error: maximum age  is {MaxAge} Years.");
+            }
+            else if (value < MinAge)
+            {
+                Console.WriteLine($"Error: minimum age  is {MinAge} Year.");
+            }
+            else
+                age = value;
         }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Person GrandPa = new Person();
+        GrandPa.Age = 99;
+        Console.WriteLine($"GrandPa.Age = {GrandPa.Age}");
+        Person GrandMa = new Person();
+        GrandMa.Age = 125;
+        Console.WriteLine($"GrandMa.Age = {GrandMa.Age}");
+
+        Person GrandSon = new Person();
+        GrandSon.Age = 1;
+        Console.WriteLine($"GrandSon.Age = {GrandSon.Age}");
+
     }
 }
