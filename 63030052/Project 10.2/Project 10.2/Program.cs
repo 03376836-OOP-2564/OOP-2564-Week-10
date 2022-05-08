7611febcdf7e48a18fcf5bdd3f2895964212f784
Piyachat <63030052@kmitl.ac.th>
2022-05-08 19:05:41 +0700

    add code

--- 63030052/Project 10.2/Project 10.2/Program.cs
+++ 63030052/Project 10.2/Project 10.2/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Project_10._2
+class Student
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	static public int id;
+	static public void PrintID()
+	{
+		Console.WriteLine($"student ID = {id}");
+	}
+
+}
+class Program
+{
+	static void Main()
+	{
+		// เรียกใช้ได้โดยไม่ต้องสร้าง instance ของ Student
+		Student.id = 1001;
+		Student.PrintID();
+	}
 }
