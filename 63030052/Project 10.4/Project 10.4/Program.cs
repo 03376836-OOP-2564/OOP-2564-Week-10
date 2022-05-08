7d7ebe1ec30c2c91aa4d9ffbb54f99c676f38335
Piyachat <63030052@kmitl.ac.th>
2022-05-08 19:19:31 +0700

    add code

--- 63030052/Project 10.4/Project 10.4/Program.cs
+++ 63030052/Project 10.4/Project 10.4/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Project_10._4
+class Cat
 {
-    internal class Program
+    private float age;
+    private string color;
+    public float Age
     {
-        static void Main(string[] args)
-        {
-        }
+        get { return age; }
+        set { age = value; }
+    }
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        // Create Cat's instances named Garfield and Thomas
+        Cat Garfield = new Cat();
+        Cat Thomas = new Cat();
+
+        // Set properties of Garfield 
+        Garfield.Age = 3.2f;
+        Garfield.Color = "Orange";
+        Thomas.Age = 2.5f;
+        Thomas.Color = "Grey";
+
+        //Show Cats  prpoerties
+        Console.WriteLine($"Garfield.Age = {Garfield.Age}");
+        Console.WriteLine($"Garfield.Color = {Garfield.Color}");
+        Console.WriteLine($"Thomas.Age = {Thomas.Age}");
+        Console.WriteLine($"Thomas.Color = {Thomas.Color}");
     }
 }
