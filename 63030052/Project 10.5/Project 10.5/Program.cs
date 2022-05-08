d4f7313bb8a810ccaed775109f4993effff1d93c
Piyachat <63030052@kmitl.ac.th>
2022-05-08 19:28:06 +0700

    add code

--- 63030052/Project 10.5/Project 10.5/Program.cs
+++ 63030052/Project 10.5/Project 10.5/Program.cs
@@ -26,6 +26,11 @@ class Program
         TrafficAmberLight.Color = "Amber";
         TrafficRedLight.Color = "Red";
 
+        // กำหนดแรงดันให้หลอดไฟจราจร
+        TrafficGreenLight.Voltage = 220;
+        TrafficAmberLight.Voltage = 220;
+        TrafficRedLight.Voltage = 220;
+
         Console.WriteLine($"Traffic light #1 : color = {TrafficGreenLight.Color}, Voltage = {TrafficGreenLight.Voltage} V.");
         Console.WriteLine($"Traffic light #2 : color = {TrafficAmberLight.Color}, Voltage = {TrafficAmberLight.Voltage} V.");
         Console.WriteLine($"Traffic light #3 : color = {TrafficRedLight.Color}, Voltage = {TrafficRedLight.Voltage} V.");
