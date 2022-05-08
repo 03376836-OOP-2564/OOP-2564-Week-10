4866b796046652c34c287bb2b88fb5deeacd034f
Piyachat <63030052@kmitl.ac.th>
2022-05-08 18:56:54 +0700

    add code

--- 63030052/Project 10.1/Project 10.1/Program.cs
+++ 63030052/Project 10.1/Project 10.1/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Project_10._1
+namespace method_examples
 {
-    internal class Program
+    class Student
     {
-        static void Main(string[] args)
+        int id;                 // instance member
+        static string name;    // static member
+        internal void SetId(int value)
         {
+            id = value;
+            ShowId();
+        }
+        internal void Set(string value)
+        {
+            name = value;
+            Show();
+        }
+
+        internal void ShowId()
+        {
+            Console.WriteLine($"id : hashcode = [{this.id.GetHashCode():X}], value = {id}");
+        }
+
+        internal unsafe void Show()
+        {
+            Console.WriteLine($"name : hashcode = [{name.GetHashCode():X}], value = {name}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            //  สร้าง instance "s1"
+            Student s1 = new Student();
+            Console.WriteLine($"Instance 's1' Hashcode = {s1.GetHashCode():X8}");
+
+            //  สร้าง instance "s2"
+            Student s2 = new Student();
+            Console.WriteLine($"Instance 's2' Hashcode = {s2.GetHashCode():X8}");
+
+            //  สร้าง instance "s3"
+            Student s3 = new Student();
+            Console.WriteLine($"Instance 's3' Hashcode = {s3.GetHashCode():X8}");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s1
+            s1.SetId(1001);
+            s1.Set("Computer Engineering");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s2
+            s2.SetId(1002);
+            s2.Set("Electrical Engineering");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s3
+            s3.SetId(1003);
+            s3.Set("Mechanical Engineering");
+            Console.WriteLine();
+
+            //  แสดงค่าใน static member ของ s1-s3 อีกครั้ง
+            Console.Write("S1."); s1.Show();
+            Console.Write("S2."); s2.Show();
+            Console.Write("S3."); s3.Show();
         }
     }
 }
