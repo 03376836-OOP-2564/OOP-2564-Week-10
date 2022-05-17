35d8137bfc014c03903940f6230e9acd799904f5
sorawitz <63030086@kmitl.ac.th>
2022-05-17 23:37:59 +0700

    63030086

---
+++ 63030086/ject10.2/ject10.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PJ_10._2
+{
+	class Student
+	{
+		public int id;
+		public void PrintID()
+		{
+			Console.WriteLine($"student ID = {id}");
+		}
+
+	}
+	class Program
+	{
+		static void Main()
+		{
+			// เรียกใช้ได้โดยสร้าง instance ของ Student
+			Student stu = new Student();
+			stu.id = 1002;
+			stu.PrintID();
+		}
+	}
+}
+
