ef0a7c7081ebdbc02b941ec399e1e15226dfec49
Tobio <92078869+Pimlapus@users.noreply.github.com>
2022-05-19 22:45:17 +0700

    Project

---
+++ 63030065/Project10.2/Project10.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Student
+{
+	 public int id;
+	 public void PrintID()
+	{
+		Console.WriteLine($"student ID = {id}");
+	}
+
+}
+class Program
+{
+	static void Main()
+	{
+		// เรียกใช้ได้โดยสร้าง instance ของ Student
+		Student stu = new Student();
+		stu.id = 1002;
+		stu.PrintID();
+	}
+}
