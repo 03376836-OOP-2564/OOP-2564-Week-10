adaf64ab90404dc90f89f13fc25dedbaf2c5ab93
AomKusuma005 <63030005@kmitl.ac.th>
2022-05-22 00:54:34 +0700

    Add By 63030005

---
+++ 63030005/Instance ans Static members/Project10.2-2/Project10.2-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Student
+{
+	static public int id;
+	static public void PrintID()
+	{
+		Console.WriteLine($"student ID = {id}");
+	}
+
+}
+class Program
+{
+	static void Main()
+	{
+		// เรียกใช้ได้โดยสร้าง instance ของ Student
+		Student stu = new Student();
+		Student.id = 1002;
+		Student.PrintID();
+	}
+}
