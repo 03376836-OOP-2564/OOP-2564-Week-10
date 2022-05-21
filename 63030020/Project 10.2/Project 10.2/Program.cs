4bc2adcb3f9f641e6d380f53f1447a3f3f49d87c
Chutipon01 <arm9423@gmail.com>
2022-05-21 19:54:47 +0700

    add 63030020

---
+++ 63030020/Project 10.2/Project 10.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_10._2
+{
+	class Student
+	{
+		static public int id;
+		static public void PrintID()
+		{
+			Console.WriteLine($"student ID = {id}");
+		}
+
+	}
+	class Program
+	{
+		static void Main()
+		{
+			// เรียกใช้ได้โดยไม่ต้องสร้าง instance ของ Student
+			Student.id = 1001;
+			Student.PrintID();
+		}
+	}
+}
