c71b960a54268c63a652af78c8d86442df506ef1
USER NAME <USER EMAIL ADDRESS>
2022-05-21 21:15:26 +0700

    Add work 029

---
+++ 63030029/Instance vs Static member/Project10.1/Project10.1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Project_10_1
+{
+    class Student
+    {
+        int id;                 // instance member
+        static string   Ford ;    // static member
+        internal void SetId(int value)
+        {
+            id = value;
+            ShowId();
+        }
+        internal void SetFord(string value)
+        {
+             Ford = value;
+             ShowFord();
+        }
+
+        internal void ShowId()
+        {
+            Console.WriteLine($"id : hashcode = [{this.id.GetHashCode():X}], value = {id}");
+        }
+
+        internal  void ShowFord()
+        {
+            Console.WriteLine($"Ford : hashcode = [{Student.Ford.GetHashCode():X}], value = {Ford}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            //  สร้าง instance "s1"
+            Student s1 = new Student();
+            Console.WriteLine($"Instance 's1' Hashcode = {s1.GetHashCode():X8}");
+
+            //  สร้าง instance "s2"
+            Student s2 = new Student();
+            Console.WriteLine($"Instance 's2' Hashcode = {s2.GetHashCode():X8}");
+
+            //  สร้าง instance "s3"
+            Student s3 = new Student();
+            Console.WriteLine($"Instance 's3' Hashcode = {s3.GetHashCode():X8}");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s1
+            s1.SetId(1001);
+            s1.SetFord("Computer Engineering");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s2
+            s2.SetId(1002);
+            s2.SetFord("Electrical Engineering");
+            Console.WriteLine();
+
+            //  กำหนดและแสดงค่าใน member ของ s3
+            s3.SetId(1003);
+            s3.SetFord("Mechanical Engineering");
+            Console.WriteLine();
+
+            //  แสดงค่าใน static member ของ s1-s3 อีกครั้ง
+            Console.Write("S1."); s1.ShowFord();
+            Console.Write("S2."); s2.ShowFord();
+            Console.Write("S3."); s3.ShowFord();
+        }
+    }
+}
